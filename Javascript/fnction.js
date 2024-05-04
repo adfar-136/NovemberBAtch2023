33002c33d8505698404a299aca46761c4b7ae159
adfar-136 <adfarrasheed136@gmail.com>
2024-05-04 22:55:14 +0530

    javascript started

---
+++ Javascript/fnction.js
@@ -0,0 +1,6 @@
+var a = 10;
+function sum(){
+    return "adfd"
+}
+var v = sum()
+console.log(v)
---
+++ Javascript/index.js
@@ -0,0 +1,19 @@
+var a = 10;
+console.log(a);
+var a = "Adfarrrr";
+console.log(a)
+a=true;
+console.log(a)
+let b = "adfar";
+console.log(b)
+b = 10
+console.log(b)
+const c = true;
+console.log(c);
+c =10;
+console.log(c);
+
+var str = "adt34.56";
+console.log(typeof(str))
+var numm = parseFloat(str);
+console.log(numm)
