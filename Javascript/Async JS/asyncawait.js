2ad570529602008bdf2e19f0b3367393c31d7ab4
adfar-136 <adfarrasheed136@gmail.com>
2024-06-05 22:57:26 +0530

    axios get putr post done

--- Javascript/Async JS/asyncawait.js
+++ Javascript/Async JS/asyncawait.js
@@ -1,30 +1,33 @@
-// async function greet(){
-//     return "adfar"
-// }
-// console.log(greet())
-// greet().then((res)=>{
-//        console.log(res)
-// })
-
-console.log("1")
- var promise = new Promise((res,rej)=>{
-    setTimeout(()=>{
-        res("Any Data")
-    },5000)
-    
- })
-
-async function executePromise(){
-    console.log("adfar")
-    // promise.then((res)=>{
-    //     console.log(res)
-    // })
-    var data =await promise
-    console.log(data)
-    console.log("error")
+async function greet(){
+    return "adfar"
 }
-executePromise()
-console.log("2")
+greet().then((result)=>{
+    console.log(result)
+})
+
+// // console.log(greet())
+// // greet().then((res)=>{
+// //        console.log(res)
+// // })
+
+// console.log("1")
+//  var promise = new Promise((res,rej)=>{
+//     setTimeout(()=>{
+//         res("Any Data")
+//     },5000)  
+//  })
+
+// async function executePromise(){
+//     console.log("adfar")
+//     // promise.then((res)=>{
+//     //     console.log(res)
+//     // })
+//     var data =await promise
+//     console.log(data)
+//     console.log("error")
+// }
+// executePromise()
+// console.log("2")
 
 
 
