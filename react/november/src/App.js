7a116c78b9e9ae2d52b92df526cd74bbe54eeff2
adfar-136 <adfarrasheed136@gmail.com>
2024-06-20 22:58:41 +0530

    react

---
+++ react/november/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ThemeChange from './components/ThemeChange'
+
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <ThemeChange/>
+    </div>
+  )
+}
+
+export default App
---
+++ react/novemberreact/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1>dyfggsd</h1>
+    </>
+  )
+}
+
+export default App
