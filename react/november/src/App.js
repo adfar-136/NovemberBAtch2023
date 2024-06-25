3868f1c42b0aebef38c3dddb89922b5d76eb438d
adfar-136 <adfarrasheed136@gmail.com>
2024-06-25 23:00:24 +0530

    props done, conditional rendering, forms

--- react/november/src/App.js
+++ react/november/src/App.js
@@ -1,34 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function App() {
-  var fName = "Adfarrrr"
+  const [firstName,setFirstName] = useState("");
+  const [allow,setAllow] = useState(false)
+  const [email,setEmail]= useState("")
+  console.log(firstName)
+  function handleSubmit(e){
+    e.preventDefault()
+     setAllow(true)
+  }
   return (
     <div>
-      <h1 >App Parent</h1>
-      <Children firstName={fName} salary="Rs4000" skill = {[1,2,3,4]} age = {34}/>
-      {/* <Children firstName="Ankit" salary="Rs2000" age={44} /> */}
-    </div>
-  )
-}
-function Children(props){
-  console.log("props: " ,props)
-  return (
-    <div>
-      {props.skill.map((item,index)=>{
-        return <h1 key={index}>{item}</h1>
-      })}
-      <h1>Children </h1>
-      {/* <p>My First Name is {props.firstName}, my age is {props.age}, and my salary is {props.salary}</p> */}
-      <Grandchildren fName={props.firstName} sal={props.salary} umar={props.age}/>
-    </div>
-  )
-}
-function Grandchildren(props){
-  console.log("p",props)
-  return (
-    <div>
-      <mark>{props.fName} {props.sal} {props.umar}</mark>
-      <h1>Grand Children</h1>
+      <h1>React Controlled Forms</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder='enter Name'
+        value={firstName} onChange={(e)=>setFirstName(e.target.value)}/><br /><br />
+        <input type="email" placeholder='enter Email'
+        value={email}/> <br /> <br />
+         
+        <input type="submit" value="Submit" />
+        <button onClick={()=>setFirstName("hello")}>hello</button>
+      </form>
+      {allow && <h1>hello {firstName}</h1>}
     </div>
   )
 }
