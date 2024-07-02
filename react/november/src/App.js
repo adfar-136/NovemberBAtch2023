e3efea3fef4424ccb4e6e401ae07a8c815caa203
adfar-136 <adfarrasheed136@gmail.com>
2024-07-02 22:32:05 +0530

    useEffect done

--- react/november/src/App.js
+++ react/november/src/App.js
@@ -1,34 +1,27 @@
 import React, { useEffect, useState } from 'react'
+import Item1 from './components/Item1'
 
 export default function App() {
-  
-  const [count,setCount] = useState(0)
+  const [condition,setCoondition] = useState(true)
   const [number,setNumber] = useState(100)
-  const [number1,setNumber1] = useState(1000)
+  const [count,setCount] = useState(0)
   useEffect(()=>{
-    console.log("useEffect")
-    // const fun =()=>{
-    //   console.log("adfar")
-    // }
-    // document.addEventListener("click",fun)
-    // return ()=>{
-    //   document.removeEventListener("click",fun)
-    // }
-   let interval =  setInterval(()=>{
-    console.log("Adfar")
-  },3000)
-  return ()=>{
-    clearInterval(interval)
-  }
-  })
+    const interval = setInterval(()=>{
+      setCount(count+1)
+    },1000)
+    return ()=>{
+      clearInterval(interval)
+    }
+  },[count])
   return (
     <div>
-       <h1>count: {count}</h1>
-       <button onClick={()=>setCount(count+1)}>Increment</button>
-       <h1>Number: {number}</h1>
-       <button onClick={()=>setNumber(number-1)}>Decrement</button>
-       <h1>Number1: {number1}</h1>
-       <button onClick={()=>setNumber1(number1+10)}>Decrement</button>
+      <button onClick={()=>setCoondition(!condition)}>Click</button>
+      <h1>Adfar Rasheed</h1>
+      <h1>Count: {count}</h1>
+      <button onClick={()=>setCount(count+1)}>ADD</button>
+      <h1>number: {number}</h1>
+      <button onClick={()=>setNumber(number+10)}>Change Number</button>
+      {condition && <Item1/>}
     </div>
   )
 }
