c4729281d16993590e795466cedf4c060a4bc4d2
adfar-136 <adfarrasheed136@gmail.com>
2024-07-04 22:57:53 +0530

    api fetching

--- react/november/src/App.js
+++ react/november/src/App.js
@@ -1,27 +1,20 @@
 import React, { useEffect, useState } from 'react'
-import Item1 from './components/Item1'
 
 export default function App() {
-  const [condition,setCoondition] = useState(true)
-  const [number,setNumber] = useState(100)
-  const [count,setCount] = useState(0)
+  const [joke,setJoke]=useState({})
   useEffect(()=>{
-    const interval = setInterval(()=>{
-      setCount(count+1)
-    },1000)
-    return ()=>{
-      clearInterval(interval)
-    }
-  },[count])
+    handleJoke()
+  },[])
+  async function handleJoke(){
+   var data = await fetch('https://api.chucknorris.io/jokes/random')
+   var jsonData =await data.json();   
+   setJoke(jsonData)
+  }
   return (
     <div>
-      <button onClick={()=>setCoondition(!condition)}>Click</button>
-      <h1>Adfar Rasheed</h1>
-      <h1>Count: {count}</h1>
-      <button onClick={()=>setCount(count+1)}>ADD</button>
-      <h1>number: {number}</h1>
-      <button onClick={()=>setNumber(number+10)}>Change Number</button>
-      {condition && <Item1/>}
+       <h1>{joke.value}</h1>
+       <img src={joke.icon_url} alt="" />
+      <button onClick={handleJoke}>generateJoke</button>
     </div>
   )
 }
