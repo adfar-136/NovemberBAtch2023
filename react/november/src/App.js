724bb0dcb823bfbf27580c6d9c91e6dffe56229f
adfar-136 <adfarrasheed136@gmail.com>
2024-07-13 22:51:40 +0530

    lifecycles discussed

--- react/november/src/App.js
+++ react/november/src/App.js
@@ -1,12 +1,29 @@
-import { Component } from "react"
-import Count from "./components/classcomponents/Count"
+import React, { Component } from 'react'
+import Contact from './components/classcomponents/Contact'
 
-class App extends Component{
-  render(){
-    return <>
-      <h1>Adfar Rasheedd</h1>
-      <Count name="Hello" age={45}/>
-    </>
+export default class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      count: 0,
+      color:"red"
+    }
+    console.log("constructor")
+  }
+ 
+  render() {
+    console.log("render")
+    return (
+      <div>
+        <Contact color={this.state.color} name="Adfar"/>
+        {/* <h1>Hello Morrow</h1>
+        <h1>count : {this.state.count}</h1>
+        <button onClick={()=>this.setState({color:"Yellow"})}>Yellow</button>
+        <button onClick={()=>this.setState({color:"pink"})}>pink</button>
+        <button onClick={()=>this.setState({color:"blue"})}>blue</button>
+        <button onClick={()=>this.setState({color:"orange"})}>orange</button>
+        <button onClick={()=>this.setState({count:this.state.count+1})}>+</button> */}
+      </div>
+    )
   }
 }
-export default App
