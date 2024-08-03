ee114f4ffbdec4de729c9434e7cfeac285b35f14
adfar-136 <adfarrasheed136@gmail.com>
2024-08-03 22:54:02 +0530

    useMEmo and useCallback is done

--- react/november/src/App.js
+++ react/november/src/App.js
@@ -1,46 +1,33 @@
-import React, { useState } from 'react'
-
+import React, { useCallback, useState } from 'react'
+const x = new Set()
 export default function App() {
-    const [workDuration,setWorkduration] = useState(25)
-    const [breakDuration,setBreakduration] = useState(5)
-    const [worksecond,setWorksecond] = useState(9)
-    const [breaksecond,setBreaksecond] = useState(300)
-    const [type,setType]= useState("Work")
-    function handleSubmit(e){
-       e.preventDefault()
-       setWorksecond(workDuration*60)
-       setBreaksecond(breakDuration*60)
-
-       setType("Work")
-    }
-    const convertor=(sec)=>{
-       let m = parseInt(sec/60).toString()
-       let s = parseInt(sec%60).toString()
-       if(m.length === 1) m="0"+m;
-       if(s.length === 1) s="0"+s;
-       return m+":"+s
-    }
+  const [count,setCount]  = useState(0)
+  const [number,setNumber] = useState(10)
+  const incrementCount=useCallback(()=>{
+    setCount(count+1)
+  },[count])
+  const decrementCount=useCallback(()=>{
+    setCount(count-1)
+  },[count])
+  const incrementNumber=useCallback(()=>{
+    setNumber(number+1)
+  },[number])
+  const decrementNumber=useCallback(()=>{
+    setNumber(number-1)
+  },[number])
+  x.add(incrementCount)
+  x.add(decrementCount)
+  x.add(incrementNumber)
+  x.add(decrementNumber)
+  alert(x.size)
   return (
     <div>
-        <div>
-            <h1>{type==="Work"?convertor(worksecond):convertor(breaksecond)}</h1>
-            <h1>{type}-Time</h1>
-        </div>
-        <div>
-            <button>Start</button>
-            <button>Stop</button>
-            <button>Reset</button>
-        </div> <br /><br />
-        <div>
-            <form onSubmit={handleSubmit}>
-                <input type="number" placeholder='Work Duration'
-                value={workDuration} onChange={(e)=>setWorkduration(e.target.value)}/>
-                <input type="number" placeholder='Break Duration'
-                value={breakDuration} onChange={(e)=>setBreakduration(e.target.value)}/>
-                <input type="submit" value="Set" />
-            </form>
-        </div>
+      <h1>Count:{count}</h1>
+      <button onClick={incrementCount}>+count</button>
+      <button onClick={decrementCount}>-count</button>
+      <h1>number:{number}</h1>
+      <button onClick={incrementNumber}>+NUmber</button>
+      <button onClick={decrementNumber}>-Number</button>
     </div>
-
   )
 }
