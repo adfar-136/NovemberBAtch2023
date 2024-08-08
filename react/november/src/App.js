1680480d342166f7e0955ea9541c42a92ea5f40f
adfar-136 <adfarrasheed136@gmail.com>
2024-08-08 22:33:54 +0530

    react completed

--- react/november/src/App.js
+++ react/november/src/App.js
@@ -1,38 +1,27 @@
 import React, { useState } from 'react'
+import ErrorBoundary from './components/Efficiency/ErrorBoundary/ErrorBoundary'
+import Counterrrr from './components/Efficiency/ErrorBoundary/Counterrrr'
+import Counter from './components/CustomHoooks/Counter'
 
 export default function App() {
-  const [email,setEmail] = useState("")
-  const [password,setPasswor] = useState("")
-  const [data,setData] = useState("")
-  function handleSubmit(e){
-       e.preventDefault()
-       const user ={email,password}
-       localStorage.setItem("user",JSON.stringify(user))
-      // //  localStorage.setItem("user",JSON.stringify({email,password}))
-      //  localStorage.setItem("email",email);
-      //  localStorage.setItem("password",password)
-  }
-  function getData(){
-    const data = JSON.parse(localStorage.getItem("user"));
-    setData(data)
-  }
-  function handleRemove(){
-    // localStorage.removeItem("user")
-    localStorage.clear()
-  }
-  console.log(data)
+  const [count,setCount] = useState(0)
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <input type="email" placeholder='Enter Email' 
-        value={email} onChange={(e)=>setEmail(e.target.value)}/>
-        <input type="password" placeholder="Enter Passord" 
-        value={password} onChange={(e)=>setPasswor(e.target.value)}/>
-        <button type="submit">Save</button>
-      </form>
-      <button onClick={getData}>Get Data</button>
-      <h1>{data.email} {data.password}</h1>
-      <button onClick={handleRemove}>Remove</button>
+      <h1>Adfar Sahab</h1> <h1>Adfar Sahab</h1>
+      <h1>App counter: {count}</h1>
+      <button onClick={()=>setCount(count+1)}>App Incrementer</button>
+      <ErrorBoundary>
+        <Counterrrr/>
+        <Counter/>
+      </ErrorBoundary>
+      <ErrorBoundary>
+        <Counterrrr/>
+        <Counter/>
+      </ErrorBoundary>
+      <ErrorBoundary>
+        <Counterrrr/>
+        <Counter/>
+      </ErrorBoundary>
     </div>
   )
 }
--- react/november/src/components/CustomHoooks/Counter.js
+++ react/november/src/components/CustomHoooks/Counter.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import useCounter from './useCounter'
 
 export default function Counter() {
+  
     const {count,increment,decrement} = useCounter()
+    useEffect(()=>{
+      if(count === 5){
+          throw new Error("bas hogaya")
+      }
+  },[count])
   return (
     <div>
         <h1>Count:{count}</h1>
