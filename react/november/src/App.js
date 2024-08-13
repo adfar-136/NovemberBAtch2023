3b7300195a2e2c5d565648da6ee1f7ed9217df51
adfar-136 <adfarrasheed136@gmail.com>
2024-08-13 22:56:34 +0530

    done

--- react/november/src/App.js
+++ react/november/src/App.js
@@ -1,23 +1,15 @@
 import React from 'react'
-import Countt from './Countt'
-import { useDispatch, useSelector } from 'react-redux'
-import {setBgColor} from "./components/ReduxStore/actions"
-import TodoRedux from './TodoRedux'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './Newton_music/pages/Home'
+
 export default function App() {
-  const color = useSelector((state)=>state.color)
-  const dispatch = useDispatch()
-  // console.log(color.background)
   return (
-    <div style={{backgroundColor:color.background}}>
-      <Countt/>
-      <button onClick={()=>dispatch(setBgColor("blue"))}>Blue</button>
-      <button onClick={()=>dispatch(setBgColor("yellow"))}>Yellow</button>
-      <button onClick={()=>dispatch(setBgColor("purple"))}>Purple</button>
-      <button onClick={()=>dispatch(setBgColor("gray"))}>Gray</button>
-      <button onClick={()=>dispatch(setBgColor("pink"))}>Pink</button>
-      <button onClick={()=>dispatch(setBgColor("Orange"))}>Orange</button>
-      <br /><br />
-      <TodoRedux/>
+    <div>
+      <BrowserRouter>
+       <Routes>
+        <Route path='/' element={<Home/>}/>
+       </Routes>
+      </BrowserRouter>
     </div>
   )
 }
--- react/november/src/index.js
+++ react/november/src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 
 import App from "./App"
 import { Provider } from 'react-redux';
-import store from './components/ReduxStore/store';
+import storee from './components/Thunkstore/store';
+// import store from './components/ReduxStore/store';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
+  <Provider store={storee}>
     <App/>
   </Provider>
 );
